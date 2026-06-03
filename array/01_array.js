6087acefc3721a52325265d8488897e61198412b
dharmik-rabari-03 <dharmik161616@gmail.com>
2026-06-03 13:31:24 +0530

    array method

---
+++ array/01_array.js
@@ -0,0 +1,17 @@
+
+//syntax
+
+const fruits = ["mango", "guava", "banana", "apple"]
+
+console.log("fruit", fruits)
+
+console.log("type", typeof (fruits))
+
+
+// using another method
+
+const vegetables = new Array("ginger", "tomato", "chilli", "potato")
+
+console.log("vegetable", vegetables)
+
+console.log("type vegetables array", typeof (vegetables))
---
+++ array/02_array.js
@@ -0,0 +1,14 @@
+const vegetable = ["ginger", "chilli", "potato", "tomato",]
+
+console.log("first element :", vegetable[0])
+
+console.log("last element :", vegetable[vegetable.length - 1])
+
+vegetable[0] = "apple"
+
+console.log("modify :", vegetable)
+
+
+vegetable.push("garlic")
+
+console.log("add last element in array :", vegetable)
